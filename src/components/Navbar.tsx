1e408c839fd3a0f7b0cd73efd738296a1d9e9656
unknown <igorstojadinovic.vs@gmail.com>
2024-09-14 15:13:29 +0200

    added logout btn

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom';
 import Logo from '../assets/images/Logo.svg';
 import { useState } from 'react';
 import { clsx } from 'clsx';
+import { BsFillCircleFill } from 'react-icons/bs';
 
 const Navbar = () => {
     const [toggle, setToggle] = useState(false);
@@ -174,7 +175,7 @@ const Navbar = () => {
                     <li className='relative w-[85%] rounded-r-lg h-10 flex items-center  justify-start px-8 py-4 gap-4 hover:bg-white border-l-4 border-l-grey-900 hover:border-l-4 hover:border-l-green group transition-all ease-in-out duration-200'>
                         <Link
                             to='/'
-                            className=''
+                            className='flex items-center gap-4'
                         >
                             <svg
                                 fill=''
@@ -189,15 +190,16 @@ const Navbar = () => {
                                     className='group-hover:fill-green'
                                 />
                             </svg>
+
+                            <span className='text-preset-3 text-grey-300 group-hover:text-grey-900'>
+                                Overview
+                            </span>
                         </Link>
-                        <span className='text-preset-3 text-grey-300 group-hover:text-grey-900'>
-                            Overview
-                        </span>
                     </li>
                     <li className='relative w-[85%] rounded-r-lg h-10 flex items-center  justify-start px-8 py-4 gap-4 hover:bg-white border-l-4 border-l-grey-900 hover:border-l-4 hover:border-l-green group transition-all ease-in-out duration-200'>
                         <Link
-                            to='/'
-                            className=''
+                            to='/transactions'
+                            className='flex items-center gap-4'
                         >
                             <svg
                                 fill='none'
@@ -212,15 +214,16 @@ const Navbar = () => {
                                     className='group-hover:fill-green'
                                 />
                             </svg>
+
+                            <span className='text-preset-3 text-grey-300 group-hover:text-grey-900'>
+                                Transactions
+                            </span>
                         </Link>
-                        <span className='text-preset-3 text-grey-300 group-hover:text-grey-900'>
-                            Transactions
-                        </span>
                     </li>
                     <li className='relative w-[85%] rounded-r-lg h-10 flex items-center  justify-start px-8 py-4 gap-4 hover:bg-white border-l-4 border-l-grey-900 hover:border-l-4 hover:border-l-green group transition-all ease-in-out duration-200'>
                         <Link
-                            to='/'
-                            className=''
+                            to='/budgets'
+                            className='flex items-center gap-4'
                         >
                             <svg
                                 fill='none'
@@ -235,15 +238,15 @@ const Navbar = () => {
                                     className='group-hover:fill-green'
                                 />
                             </svg>
+                            <span className='text-preset-3 text-grey-300 group-hover:text-grey-900'>
+                                Budgets
+                            </span>{' '}
                         </Link>
-                        <span className='text-preset-3 text-grey-300 group-hover:text-grey-900'>
-                            Budgets
-                        </span>
                     </li>
                     <li className='relative w-[85%] rounded-r-lg h-10 flex items-center  justify-start px-8 py-4 gap-4 hover:bg-white border-l-4 border-l-grey-900 hover:border-l-4 hover:border-l-green group transition-all ease-in-out duration-200'>
                         <Link
-                            to='/'
-                            className=''
+                            to='/posts'
+                            className='flex items-center gap-4'
                         >
                             <svg
                                 fill='none'
@@ -258,15 +261,15 @@ const Navbar = () => {
                                     className='group-hover:fill-green'
                                 />
                             </svg>
+                            <span className='text-preset-3 text-grey-300 group-hover:text-grey-900'>
+                                Pots
+                            </span>{' '}
                         </Link>
-                        <span className='text-preset-3 text-grey-300 group-hover:text-grey-900'>
-                            Pots
-                        </span>
                     </li>
                     <li className='relative w-[85%] rounded-r-lg h-10 flex items-center  justify-start px-8 py-4 gap-4 hover:bg-white border-l-4 border-l-grey-900 hover:border-l-4 hover:border-l-green group transition-all ease-in-out duration-200'>
                         <Link
-                            to='/'
-                            className=''
+                            to='/recurring'
+                            className='flex items-center gap-4'
                         >
                             <svg
                                 fill='none'
@@ -281,52 +284,66 @@ const Navbar = () => {
                                     className='group-hover:fill-green'
                                 />
                             </svg>
+
+                            <span className='text-preset-3 text-grey-300 group-hover:text-grey-900'>
+                                Recurring Bills
+                            </span>
                         </Link>
-                        <span className='text-preset-3 text-grey-300 group-hover:text-grey-900'>
-                            Recurring Bills
-                        </span>
                     </li>
                 </ul>
-                <div
-                    className={clsx(
-                        {
-                            'flex items-center  w-[85%]  h-10 rounded-r-lg gap-4 py-2 px-10 mb-28 hover:bg-white border-l-4  border-l-grey-900 hover:border-l-green group  transition-all ease-in-out duration-75':
-                                !toggle,
-                        },
-                        {
-                            'flex items-center  w-[85%]  h-10 rounded-r-lg gap-4  mb-28 hover:bg-white border-l-4  border-l-grey-900 hover:border-l-green group  transition-all ease-in-out duration-75':
-                                toggle,
-                        }
-                    )}
-                    onClick={toggleMenu}
-                >
-                    <svg
-                        fill='none'
-                        height='20'
-                        viewBox='0 0 20 20'
-                        width='20'
-                        xmlns='http://www.w3.org/2000/svg'
-                    >
-                        <path
-                            d='m14.0001 5.26002v8.99998c0 .1989-.079.3897-.2197.5304-.1406.1406-.3314.2196-.5303.2196h-2.25v3.75c.0001.1484-.0438.2936-.1262.417-.0824.1235-.1996.2197-.3367.2765s-.288.0717-.4336.0427c-.14554-.029-.27923-.1005-.38412-.2056l-9.000003-9c-.069733-.0696-.125052-.1523-.162795-.2434-.037743-.09102-.05717-.18862-.05717-.28718s.019427-.19615.05717-.2872.093062-.17377.162795-.24342l9.000003-9.000004c.10489-.105009.23858-.1765346.38412-.2055224.1456-.02898777.2965-.0141343.4336.0426801.1371.0568143.2543.1530353.3367.2764803s.1263.268565.1262.416987v3.749999h2.25c.1989 0 .3897.07902.5303.21967.1407.14065.2197.33142.2197.53033zm2.25-.75c-.1989 0-.3897.07902-.5303.21967-.1407.14065-.2197.33142-.2197.53033v8.99998c0 .1989.079.3897.2197.5304.1406.1406.3314.2196.5303.2196s.3897-.079.5303-.2196c.1407-.1407.2197-.3315.2197-.5304v-8.99998c0-.19891-.079-.38968-.2197-.53033-.1406-.14065-.3314-.21967-.5303-.21967zm3 0c-.1989 0-.3897.07902-.5303.21967-.1407.14065-.2197.33142-.2197.53033v8.99998c0 .1989.079.3897.2197.5304.1406.1406.3314.2196.5303.2196s.3897-.079.5303-.2196c.1407-.1407.2197-.3315.2197-.5304v-8.99998c0-.19891-.079-.38968-.2197-.53033-.1406-.14065-.3314-.21967-.5303-.21967z'
-                            fill='#b3b3b3'
-                            className='group-hover:fill-green '
-                        />
-                    </svg>
-                    <span
+                <div className='flex flex-col gap-6'>
+                    <div
                         className={clsx(
                             {
-                                'text-preset-3 text-grey-300 group-hover:text-grey-900 transition-all delay-100':
+                                'flex items-center  w-[85%]  h-10 rounded-r-lg gap-4 py-2 px-10  hover:bg-white border-l-4  border-l-grey-900 hover:border-l-green group  transition-all ease-in-out duration-75':
                                     !toggle,
                             },
-
                             {
-                                hidden: toggle,
+                                'flex items-center  w-[85%]  h-10 rounded-r-lg gap-4   hover:bg-white border-l-4  border-l-grey-900 hover:border-l-green group  transition-all ease-in-out duration-75':
+                                    toggle,
                             }
                         )}
+                        onClick={toggleMenu}
                     >
-                        Minimize
-                    </span>
+                        <svg
+                            fill='none'
+                            height='20'
+                            viewBox='0 0 20 20'
+                            width='20'
+                            xmlns='http://www.w3.org/2000/svg'
+                        >
+                            <path
+                                d='m14.0001 5.26002v8.99998c0 .1989-.079.3897-.2197.5304-.1406.1406-.3314.2196-.5303.2196h-2.25v3.75c.0001.1484-.0438.2936-.1262.417-.0824.1235-.1996.2197-.3367.2765s-.288.0717-.4336.0427c-.14554-.029-.27923-.1005-.38412-.2056l-9.000003-9c-.069733-.0696-.125052-.1523-.162795-.2434-.037743-.09102-.05717-.18862-.05717-.28718s.019427-.19615.05717-.2872.093062-.17377.162795-.24342l9.000003-9.000004c.10489-.105009.23858-.1765346.38412-.2055224.1456-.02898777.2965-.0141343.4336.0426801.1371.0568143.2543.1530353.3367.2764803s.1263.268565.1262.416987v3.749999h2.25c.1989 0 .3897.07902.5303.21967.1407.14065.2197.33142.2197.53033zm2.25-.75c-.1989 0-.3897.07902-.5303.21967-.1407.14065-.2197.33142-.2197.53033v8.99998c0 .1989.079.3897.2197.5304.1406.1406.3314.2196.5303.2196s.3897-.079.5303-.2196c.1407-.1407.2197-.3315.2197-.5304v-8.99998c0-.19891-.079-.38968-.2197-.53033-.1406-.14065-.3314-.21967-.5303-.21967zm3 0c-.1989 0-.3897.07902-.5303.21967-.1407.14065-.2197.33142-.2197.53033v8.99998c0 .1989.079.3897.2197.5304.1406.1406.3314.2196.5303.2196s.3897-.079.5303-.2196c.1407-.1407.2197-.3315.2197-.5304v-8.99998c0-.19891-.079-.38968-.2197-.53033-.1406-.14065-.3314-.21967-.5303-.21967z'
+                                fill='#b3b3b3'
+                                className='group-hover:fill-green '
+                            />
+                        </svg>
+                        <span
+                            className={clsx(
+                                {
+                                    'text-preset-3 text-grey-300 group-hover:text-grey-900 transition-all ':
+                                        !toggle,
+                                },
+
+                                {
+                                    hidden: toggle,
+                                }
+                            )}
+                        >
+                            Minimize
+                        </span>
+                    </div>
+
+                    <Link
+                        to='/login'
+                        className=' group flex items-center  w-[85%]  h-10 rounded-r-lg gap-4 py-2 px-10 mb-20  hover:bg-white border-l-4  border-l-grey-900 hover:border-l-green  text-preset-3 text-grey-300 hover:text-grey-900 transition-all'
+                    >
+                        <BsFillCircleFill
+                            size={20}
+                            className='group-hover:text-green'
+                        />{' '}
+                        Logout
+                    </Link>
                 </div>
             </div>
         </nav>
