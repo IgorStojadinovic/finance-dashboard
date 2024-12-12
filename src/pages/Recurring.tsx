7482557f9be66b0baf10266c3ca68063a0952a2b
IgorST <igorstojadinovic.vs@gmail.com>
2024-12-12 14:43:20 +0100

    Add unique keys and semantic tbody tag to Recurring table
    Added unique keys for rows in the map function to avoid rendering issues and included a semantic <tbody> tag to improve HTML structure and accessibility. Cleaned up excess blank lines for better code readability.

--- src/pages/Recurring.tsx
+++ src/pages/Recurring.tsx
@@ -12,8 +12,6 @@ const RecurringBills = () => {
         <div className="w-full bg-beige-100 py-6 px-4 md:px-10 md:py-8 xl:px-10 gap-8 xl:gap-8 flex flex-col ">
             <h1 className="text-preset-1">Recurring Bills</h1>
             <section className="flex flex-col gap-3 md:flex-wrap md:flex-row md:gap-6  ">
-
-
                 <section className="xl:w-1/2 flex flex-col gap-5 w-full md:flex-row xl:flex-col">
 
                     <div
@@ -89,6 +87,7 @@ const RecurringBills = () => {
                         </Menu>
                     </div>
                     <table>
+                        <tbody>
                         <tr className="hidden md:flex justify-between text-preset-5 text-grey-300  ">
                             <th className="flex-1 text-left">Bill Title</th>
                             <th className="flex-1 text-left">Due Date</th>
@@ -96,9 +95,9 @@ const RecurringBills = () => {
                         </tr>
 
                         {
-                            userRecurringBills.map((item) => {
+                            userRecurringBills.map((item, index) => {
                                 return (
-                                    <tr className=" border-b last:border-b-0 ">
+                                    <tr className=" border-b last:border-b-0 " key={index}>
                                         <th className="py-5 flex flex-col gap-2 md:flex-row w-full md:flex md:justify-between">
                                             <div className="flex items-center gap-4 md:w-1/3 ">
                                                 <img src={item.icon} className="rounded-full h-8"/>
@@ -130,6 +129,7 @@ const RecurringBills = () => {
                                 );
                             })
                         }
+                        </tbody>
                     </table>
                 </div>
             </section>
