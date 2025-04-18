b4f0b6d9f9ac108a0d348b5957533955685882ec
IgorST <igorstojadinovic.vs@gmail.com>
2025-04-18 20:13:49 +0200

    Fixed Chart , It takes api data now

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,65 +1,74 @@
-import {Doughnut} from "react-chartjs-2";
-import {Chart as ChartJS, ArcElement, Tooltip, Legend} from "chart.js";
+import { Doughnut } from 'react-chartjs-2';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Budget } from '../lib/types/types';
 
 ChartJS.register(ArcElement, Tooltip, Legend);
-ChartJS.overrides.doughnut.cutout = "110";
+ChartJS.overrides.doughnut.cutout = '110';
 ChartJS.overrides.doughnut.radius = 110;
 
-const Chart = () => {
-    const data = {
-        labels: ["Entertainment", "Bills", "Dining Out", "Personal Care"],
-        datasets: [
-            {
-                data: [50, 750, 75, 100],
-                backgroundColor: ["#277C78", "#82C9D7", "#626070", "#F2CDAC"],
-                borderWidth: 0,
+const Chart = ({
+  budgetsData,
+  isLoading,
+}: {
+  budgetsData: Budget[];
+  isLoading: boolean;
+}) => {
+  const data = {
+    labels: budgetsData.map(budget => budget.category),
+    datasets: [
+      {
+        data: budgetsData.map(budget => budget.maximum),
+        backgroundColor: budgetsData.map(budget => budget.hex),
+        borderWidth: 0,
+      },
+    ],
+  };
+
+  return (
+    <div className='relative '>
+      <Doughnut
+        style={{ zIndex: 9000 }}
+        data={data}
+        options={{
+          responsive: true,
+          plugins: {
+            legend: {
+              display: false,
             },
-        ],
-    };
+            tooltip: {
+              usePointStyle: true,
+              callbacks: {
+                label: function (context) {
+                  let label = context.dataset.label || '';
 
-    return (
-        <div className="relative ">
-            <Doughnut
-                className="z-10"
-                data={data}
-                options={{
-                    responsive: true,
-                    plugins: {
-                        legend: {
-                            display: false,
-                        },
-                        tooltip: {
-                            usePointStyle: true,
-                            callbacks: {
-                                label: function (context) {
-                                    let label = context.dataset.label || "";
+                  if (label) {
+                    label += ': ';
+                  }
+                  if (context.parsed.valueOf() !== null) {
+                    label += new Intl.NumberFormat('en-US', {
+                      style: 'currency',
+                      currency: 'USD',
+                    }).format(context.parsed.valueOf());
+                  }
+                  return label;
+                },
+              },
+            },
+          },
+        }}
+      />
 
-                                    if (label) {
-                                        label += ": ";
-                                    }
-                                    if (context.parsed.valueOf() !== null) {
-                                        label += new Intl.NumberFormat(
-                                            "en-US",
-                                            {
-                                                style: "currency",
-                                                currency: "USD",
-                                            }
-                                        ).format(context.parsed.valueOf());
-                                    }
-                                    return label;
-                                },
-                            },
-                        },
-                    },
-                }}
-            />
-
-            <div className="absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] text-center">
-                <h3 className="text-preset-1">$338</h3>
-                <span className="text-preset-5">of $975 limit</span>
-            </div>
-        </div>
-    );
+      <div className='absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%]  text-center'>
+        <h3 className='text-preset-1 '>
+          {budgetsData.reduce((acc, budget) => acc + budget.maximum, 0)}$
+        </h3>
+        <span className='text-preset-5 '>
+          of {budgetsData.reduce((acc, budget) => acc + budget.limmit, 0)}$
+          limit
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default Chart;
