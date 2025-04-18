26a3de30f2ff6f87ab0d3cd2ef0ed5b88a57d770
IgorST <igorstojadinovic.vs@gmail.com>
2025-04-18 20:10:18 +0200

    vite ts config update

---
+++ vite.config.js
@@ -0,0 +1,31 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react()],
+    test: {
+        environment: 'jsdom',
+        globals: true,
+        setupFiles: ['./tests/setup.ts'],
+        include: ['**/*.{test,spec}.{js,jsx,ts,tsx}'],
+        exclude: ['node_modules', 'dist', '.idea', '.git', '.cache'],
+        testTransformMode: {
+            web: ['.[jt]sx'],
+        },
+        deps: {
+            inline: ['@headlessui/react'],
+        },
+    },
+    resolve: {
+        extensions: ['.js', '.ts', '.jsx', '.tsx'],
+    },
+    server: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:3000',
+                changeOrigin: true,
+            },
+        },
+    },
+});
