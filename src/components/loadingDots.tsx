004049664f534fcba319283a06d800ce10ed4fde
IgorST <igorstojadinovic.vs@gmail.com>
2025-04-18 20:08:52 +0200

    Added fulback loading component

---
+++ src/components/loadingDots.tsx
@@ -0,0 +1,56 @@
+import { motion, Variants } from 'motion/react';
+
+function LoadingThreeDotsJumping() {
+  const dotVariants: Variants = {
+    jump: {
+      y: -30,
+      transition: {
+        duration: 0.5,
+        repeat: Infinity,
+        repeatType: 'mirror',
+        ease: 'easeInOut',
+      },
+    },
+  };
+
+  return (
+    <motion.div
+      animate='jump'
+      transition={{ staggerChildren: -0.4, staggerDirection: -1 }}
+      className='container'
+    >
+      <motion.div className='dot' variants={dotVariants} />
+      <motion.div className='dot' variants={dotVariants} />
+      <motion.div className='dot' variants={dotVariants} />
+      <StyleSheet />
+    </motion.div>
+  );
+}
+
+/**
+ * ==============   Styles   ================
+ */
+function StyleSheet() {
+  return (
+    <style>
+      {`
+            .container {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                gap: 10px;
+            }
+
+            .dot {
+                width: 20px;
+                height: 20px;
+                border-radius: 50%;
+                background-color: #82C9D7;
+                will-change: transform;
+            }
+            `}
+    </style>
+  );
+}
+
+export default LoadingThreeDotsJumping;
